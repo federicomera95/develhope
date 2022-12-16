1330c068fab46d11e533e7831bc158382bc6f542
Federico Merafina <federico.merafina@losectrl.it>
2022-12-16 13:52:13 +0100

    feat: exercise 21

---
+++ es_21/exercise-21.js
@@ -0,0 +1,8 @@
+
+let [num1,num2] = [10,20];
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// Variable swapping
+[num2,num1] = [num1,num2];
+
+console.log('After swap: ', num1, num2); // After swap: 20 10
