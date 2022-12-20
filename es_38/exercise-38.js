c7c81581c8499f2c7f22ae5ca2069326efe1abb7
Federico Merafina <federico.merafina@losectrl.it>
2022-12-20 17:07:14 +0100

    feat: add exercise 38

---
+++ es_38/exercise-38.js
@@ -0,0 +1,31 @@
+const isLogged = true;
+
+function firstPromise(isLogged){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if(isLogged){
+                let rand = Math.random();
+                resolve(rand)
+            }else{
+                reject(new Error("Not logged in!"))
+            }
+        }, 500);
+    })
+}
+
+function secondPromise(rand){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if(rand > 0.5){
+                resolve({name: "John", age: 24})
+            }else{
+                reject(new Error("rand is lower than 0.5!"))
+            }
+        }, 500);
+    })
+}
+
+firstPromise(isLogged)
+    .then((rand)=> secondPromise(rand))
+    .then((user) => console.log(user))
+    .catch((err) => console.log(err))
