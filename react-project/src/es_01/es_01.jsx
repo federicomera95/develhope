48087299f6197c30092a42e2f3920023ec0dbd61
Federico Merafina <federico.merafina@losectrl.it>
2023-01-26 11:49:22 +0100

    fix: root and ad ex_02

--- react-project/src/es_01/es_01.jsx
+++ react-project/src/es_01/es_01.jsx
@@ -4,8 +4,12 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const hello = 'Hello World!!!';
+
 root.render(
   <React.StrictMode>
-    
+    <h1>
+    {hello}
+    </h1>
   </React.StrictMode>
 );
--- react-project/src/es_02/es_02.jsx
+++ react-project/src/es_02/es_02.jsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const sayMyName = (name)=> <h1>Hello, {name}</h1>;
+
 root.render(
   <React.StrictMode>
-    
+    {sayMyName('Federico')}
   </React.StrictMode>
 );
