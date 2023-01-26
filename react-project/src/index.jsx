1e615f9881b2b81e3b92873bdf48033fa06c196b
Federico Merafina <federico.merafina@losectrl.it>
2023-01-26 11:38:19 +0100

    feat: add ex jsx 01

--- react-project/src/index.jsx
+++ react-project/src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Hello = 'Hello World!!!';
+
 root.render(
   <React.StrictMode>
-    <App />
+      <h1>
+        {Hello}
+      </h1>
   </React.StrictMode>
 );
 
