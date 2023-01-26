370956bb71469d9a49480294f86ef341bedab6db
Federico Merafina <federico.merafina@losectrl.it>
2023-01-26 12:08:17 +0100

    feat: add ex 07

---
+++ react-project/src/es_06/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Hello } from "../es_05/hello";
+
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Hello />
+            </div>
+        );
+    }
+}
