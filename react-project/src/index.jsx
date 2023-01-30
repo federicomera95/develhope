ec57883860ffca40f6440e393a4ef44251fc17a0
Federico Merafina <federico.merafina@losectrl.it>
2023-01-30 15:19:12 +0100

    feat: es_20

--- react-project/src/index.jsx
+++ react-project/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './es_18/App';
+import { App } from './es_20/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
