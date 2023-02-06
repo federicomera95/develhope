75c41f044a7355964616da30648aa628e562631f
Federico Merafina <federico.merafina@losectrl.it>
2023-02-06 15:23:50 +0100

    feat: add es_33

---
+++ react-project/src/es_33/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export class TodoList extends React.Component {
+
+    items=['javascript','php','python','java'];
+
+    handleAddNewItem = ()=> {
+       this.setState((items)=>{
+            this.items.push(document.getElementById('item').value);
+            document.getElementById('item').value = '';
+            return this.items;
+       });
+    }
+
+    render() {
+        return (
+             <ul>
+                {this.items.map((name,index)=> <li key={name+index}>{name}</li>)}
+                <input type="text" id="item"/>
+                <button onClick={this.handleAddNewItem}>Add item</button>
+             </ul>
+        );
+    }
+}
--- react-project/src/index.jsx
+++ react-project/src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { App } from './es_32/App';
+import { App } from './es_33/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
