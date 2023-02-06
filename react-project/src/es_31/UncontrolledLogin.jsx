a474da49b7da4ae4f1e4f253633c2db1c75c2177
Federico Merafina <federico.merafina@losectrl.it>
2023-02-06 14:58:47 +0100

    feat: es_31

---
+++ react-project/src/es_31/UncontrolledLogin.jsx
@@ -0,0 +1,34 @@
+import React, { createRef } from "react";
+
+
+export class UncontrolledLogin extends React.Component {
+
+    _formRef = createRef() 
+
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        const username = event.target.elements.username.value; 
+        const password = event.target.elements.password.value;
+        const remember = event.target.elements.remember.checked;
+
+        console.log({
+            username,
+            password,
+            remember
+        });
+    }
+
+    render() {
+        return (
+            <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+                <input type="text" name="username" autoFocus/>
+                <input type="password" name="password"/>
+                <input type="checkbox" name="remember" />
+                <button type="submit">Login</button>
+                <button type="reset">Reset</button>
+            </form>
+            
+        );
+    }
+}
--- react-project/src/index.jsx
+++ react-project/src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { App } from './es_30/App';
+import { App } from './es_31/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
