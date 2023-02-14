9b80abef27a880ce692e381ad599cdb78b85e7f3
Federico Merafina <federico.merafina@losectrl.it>
2023-02-14 12:26:30 +0100

    feat: es_37

---
+++ react-project/src/es_37/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export class Login extends React.Component {
+
+    state = { username: '', password: '', remember: false};
+
+    handleInputChange = ({name, value}) => {
+        this.setState({ [name]: value });
+    }
+
+    handleCheckboxChange = ({value}) => {
+        this.setState({ remember: value});
+    }
+
+    render() {
+        return (
+             <div>
+                <input type="text" name="username" onChange={({target}) => this.handleInputChange(target)} value={this.state.username}/>
+                <input type="password" name="password" onChange={({target}) => this.handleInputChange(target)} value={this.state.password}/>
+                <input type="checkbox" name="remember" onChange={({target}) => this.handleCheckboxChange(target)} checked={this.state.remember}/>
+                <button style={{ backgroundColor: this.state.password.length <= 8 ? 'red': 'green' }} disabled={this.state.username === '' || this.state.password === ''} onClick={this.props.onLogin}>Login</button>
+            </div>
+            
+        );
+    }
+}
