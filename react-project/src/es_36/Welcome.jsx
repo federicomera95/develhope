9071239c4739b484ceff903b133d6a658435bcac
Federico Merafina <federico.merafina@losectrl.it>
2023-02-14 12:20:56 +0100

    feat: add es_36

---
+++ react-project/src/es_36/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export class Welcome extends React.Component {
+
+    render() {
+        return (
+            <div className='welcome'>
+                <h1>Hello world!</h1>
+            </div>
+        )
+    }
+}
