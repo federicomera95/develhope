110530c73a474b2f13f71710de96f2297fe8d313
Federico Merafina <federico.merafina@losectrl.it>
2023-02-21 11:54:03 +0100

    feat: add index for es_60

--- react-project/src/index.jsx
+++ react-project/src/index.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import App from './es_53/App';
+import { BrowserRouter } from "react-router-dom";
+import App from './es_60/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
- <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
