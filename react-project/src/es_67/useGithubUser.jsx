8c34e55590edc6b5d55061086dfe89ca0cf1dfd2
Federico Merafina <federico.merafina@losectrl.it>
2023-02-21 12:59:13 +0100

    feat: es_67

---
+++ react-project/src/es_67/useGithubUser.jsx
@@ -0,0 +1,13 @@
+import useSWR  from "swr";
+
+const fetcher = url => fetch(url).then(response => response.json())
+
+const useGithubUser = ({username}) => {
+   
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
+
+    return { user: data, error, isLoading: !data && !error };
+   
+}
+
+export default useGithubUser;
