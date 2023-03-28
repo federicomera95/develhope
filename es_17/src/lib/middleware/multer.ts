eab2839691d89deb9e5ead683b0a2dfc548ffdea
Federico Merafina <federico.merafina@losectrl.it>
2023-03-28 13:29:39 +0200

    feat: add route post upload file and config multer and mime to save file to disk

--- es_17/src/lib/middleware/multer.ts
+++ es_17/src/lib/middleware/multer.ts
@@ -1,7 +1,20 @@
 import multer from "multer";
+import { randomUUID } from "crypto";
+import mime from "mime";
+
+export const generatePhotoFileName = (mimeType: string) => {
+  const randomFileName = `${randomUUID()}-${Date.now()}`;
+  const fileExtension = mime.getExtension(mimeType);
+  const fileName = `${randomFileName}.${fileExtension}`;
+
+  return fileName;
+};
 
 const storage = multer.diskStorage({
   destination: "uploads/",
+  filename: (req, file, callback) => {
+    return callback(null, generatePhotoFileName(file.mimetype));
+  },
 });
 
 export const multerOptions = {};
